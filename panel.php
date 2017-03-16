2642a96addbcf944b06e4d1badeb5b78d6ab8a37
angelslv <angel.vasquez@tecnologiasmexico.com>
2017-03-16 12:38:56 -0600

    Creacion de los archivos panel.php y style-panel-admin.css con fecha 16 de marzo de 2017 -angelslv

---
+++ panel.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Panel Administrativo</title>
+    <meta charset="UTF-8" />
+    <meta name="description" content="Práctica 2 - TIII" />
+    <meta name="keywords" content="HTML5, CSS3, GIT, GITHUB, PHP, MARIADB " />
+    <meta name="author" content="Dr. Ángel Vásquez" />
+    <link rel="stylesheet" href="css/style-panel-admin.css" type="text/css" />
+    
+     <!-- INICIAN ETIQUETAS NUEVAS -->
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1" />
+    <meta name="robots" content="index, follow" />
+    <!-- TERMINAN ETIQUETAS NUEVAS -->
+</head>
+<body>
+<?php
+$usuario = $_POST['v_user'];
+$contrasenia = $_POST['v_password'];
+
+if( $usuario=='angelslv' && $contrasenia=='admin')
+{
+    echo "Bienvenido Dr. Ángel Vásquez";
+    echo "<a href='index.html'>Salir</a>";
+}
+else{
+    echo "Usuario incorrecto";
+    header('location:index.html');
+}
+?>
+</body>
+</html>
